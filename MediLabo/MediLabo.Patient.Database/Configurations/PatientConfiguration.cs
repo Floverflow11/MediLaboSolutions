a086406f0b4d8ff7aebbc1d6741f334dabc84674
Floverflow11 <flo.cinbolat@gmail.com>
2026-05-05 14:52:52 +0200

    feat(patient-db): add patient entity, ef configuration and seed data

---
+++ MediLabo/MediLabo.Patient.Database/Configurations/PatientConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MediLabo.Patient.Database.Configurations;
+
+public class PatientConfiguration : IEntityTypeConfiguration<Domain.Patient>
+{
+    public void Configure(EntityTypeBuilder<Domain.Patient> builder)
+    {
+        builder.Property(p => p.FirstName).HasMaxLength(64).IsRequired();
+        builder.Property(p => p.LastName).HasMaxLength(64).IsRequired();
+        builder.Property(p => p.Address).HasMaxLength(128);
+        builder.Property(p => p.PhoneNumber).HasMaxLength(32);
+    }
+}
---
+++ MediLabo/MediLabo.Patient.Domain/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace MediLabo.Patient.Domain;
+
+public class Patient
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateOnly DateOfBirth { get; set; }
+    public char Gender { get; set; }
+    public string? Address { get; set; }
+    public string? PhoneNumber { get; set; }
+}
