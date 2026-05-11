fa8761efddf81bab4fc65ba7363c8806d107445d
Floverflow11 <flo.cinbolat@gmail.com>
2026-05-11 15:39:31 +0200

    feat(patient-api): add minimal api crud endpoints and http test file

--- MediLabo/MediLabo.Patient.API/Program.cs
+++ MediLabo/MediLabo.Patient.API/Program.cs
@@ -1,4 +1,5 @@
 using MediLabo.Patient.Database;
+using MediLabo.Patient.Domain;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,5 +16,62 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+var patientGroup = app.MapGroup("/patients");
+
+patientGroup.MapGet("/",
+    async (PatientDbContext context) => Results.Ok(await context.Patients.AsNoTracking().ToListAsync()));
+patientGroup.MapGet("/{id:int}", async (int id, PatientDbContext context) =>
+{
+    var patient = await context.Patients.FindAsync(id);
+
+    return patient == null ? Results.NotFound() : Results.Ok(patient);
+});
+patientGroup.MapPost("/", async (Patient patient, PatientDbContext context) =>
+{
+    context.Patients.Add(patient);
+    await context.SaveChangesAsync();
+
+    return Results.Created($"/patients/{patient.Id}", patient);
+});
+patientGroup.MapPut("/{id:int}", async (int id, Patient patient, PatientDbContext context) =>
+{
+    if (id != patient.Id)
+    {
+        return Results.BadRequest("Parameter id and patient id mismatch");
+    }
+
+    var existingPatient = await context.Patients.FindAsync(id);
+
+    if (existingPatient == null)
+    {
+        return Results.NotFound();
+    }
+
+    existingPatient.FirstName = patient.FirstName;
+    existingPatient.LastName = patient.LastName;
+    existingPatient.DateOfBirth = patient.DateOfBirth;
+    existingPatient.Gender = patient.Gender;
+    existingPatient.Address = patient.Address;
+    existingPatient.PhoneNumber = patient.PhoneNumber;
+
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+patientGroup.MapDelete("/{id:int}", async (int id, PatientDbContext context) =>
+{
+    var patient = await context.Patients.FindAsync(id);
+
+    if (patient == null)
+    {
+        return Results.NotFound();
+    }
+
+    context.Patients.Remove(patient);
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
 
 app.Run();
