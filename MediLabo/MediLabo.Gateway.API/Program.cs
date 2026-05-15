928654f87895c6e9d85d3cad21831d017801671f
Floverflow11 <flo.cinbolat@gmail.com>
2026-05-15 16:44:09 +0200

    feat(gateway-db): setup in-memory identity database and seed admin user

--- MediLabo/MediLabo.Gateway.API/Program.cs
+++ MediLabo/MediLabo.Gateway.API/Program.cs
@@ -1,12 +1,24 @@
+using MediLabo.Gateway.Database;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AuthDbContext>(options => options.UseInMemoryDatabase("Users"));
+builder.Services.AddIdentityCore<IdentityUser>().AddEntityFrameworkStores<AuthDbContext>();
+
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddOcelot();
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
+
+await using var scope = app.Services.CreateAsyncScope();
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+var user = new IdentityUser { UserName = "admin", Email = "admin@medilabo.com" };
+await userManager.CreateAsync(user, "Admin123!");
+
 await app.UseOcelot();
 
 app.Run();
