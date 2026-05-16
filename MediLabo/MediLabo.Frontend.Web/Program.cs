04c722850e25b4484a1ce3589f6d15607bd12434
Floverflow11 <flo.cinbolat@gmail.com>
2026-05-16 16:20:06 +0200

    feat(gateway): add basic authentication scheme

--- MediLabo/MediLabo.Frontend.Web/Program.cs
+++ MediLabo/MediLabo.Frontend.Web/Program.cs
@@ -1,10 +1,17 @@
+using System.Net.Http.Headers;
+using System.Text;
 using MediLabo.Frontend.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient<IPatientService, PatientService>(client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration["Services:GatewayBaseUrl"]!);
+    var gatewayConfig = builder.Configuration.GetSection("Gateway");
+    client.BaseAddress = new Uri(gatewayConfig["BaseUrl"]!);
+    var username = gatewayConfig["username"]!;
+    var password = gatewayConfig["password"]!;
+    var authString = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}"));
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authString);
 });
 
 var app = builder.Build();
--- MediLabo/MediLabo.Gateway.API/Program.cs
+++ MediLabo/MediLabo.Gateway.API/Program.cs
@@ -1,4 +1,6 @@
+using MediLabo.Gateway.API.Security;
 using MediLabo.Gateway.Database;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Ocelot.DependencyInjection;
@@ -8,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AuthDbContext>(options => options.UseInMemoryDatabase("Users"));
 builder.Services.AddIdentityCore<IdentityUser>().AddEntityFrameworkStores<AuthDbContext>();
+builder.Services.AddAuthentication("BasicAuth")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuth", null);
 
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddOcelot();
 builder.Services.AddOcelot(builder.Configuration);
@@ -16,8 +20,17 @@ var app = builder.Build();
 
 await using var scope = app.Services.CreateAsyncScope();
 var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-var user = new IdentityUser { UserName = "admin", Email = "admin@medilabo.com" };
-await userManager.CreateAsync(user, "Admin123!");
+
+var adminConfig = app.Configuration.GetSection("Admin");
+var username = adminConfig["Username"]!;
+var password = adminConfig["Password"]!;
+var email = adminConfig["Email"]!;
+
+var user = new IdentityUser { UserName = username, Email = email };
+await userManager.CreateAsync(user, password);
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 await app.UseOcelot();
 
