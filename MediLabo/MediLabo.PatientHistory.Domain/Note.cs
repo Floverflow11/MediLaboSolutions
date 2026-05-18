c876e8f365b94238676df883beb194992ed8eb5b
Floverflow11 <flo.cinbolat@gmail.com>
2026-05-18 16:20:31 +0200

    feat(history-db): setup mongodb context and seed initial notes

---
+++ MediLabo/MediLabo.PatientHistory.Domain/Note.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MediLabo.PatientHistory.Domain;
+
+public record Note
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = null!;
+    public int PatientId { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
